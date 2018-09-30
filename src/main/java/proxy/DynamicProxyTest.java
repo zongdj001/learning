91c2e413bf4bd88b3a00a056abe2ab9c913cc5c2
zongdj <zongdj@cn.ibm.com>
2018-09-30 15:51:48 +0800

    use gradle

---
+++ src/main/java/proxy/DynamicProxyTest.java
@@ -0,0 +1,18 @@
+package proxy;
+
+import java.lang.reflect.Proxy;
+
+public class DynamicProxyTest {
+    public static void main(String[] args) {
+        Hello hello = new HelloImp();
+        
+        Hello helloProxy = (Hello)Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[] {Hello.class}, new LogInvocationhandler(hello));
+        String result = helloProxy.sayHello("zdj");
+        
+        System.out.println("proxy returned result is : " + result);
+        //toString, hasCode, equals, same behavior with sayHello
+        System.out.println(hello.toString());
+        //it's will not go proxy
+        System.out.println(helloProxy.getClass());
+    }
+}
---
+++ src/main/java/proxy/Hello.java
@@ -0,0 +1,5 @@
+package proxy;
+
+public interface Hello {
+    String sayHello(String toName);
+}
